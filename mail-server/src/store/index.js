3dcf6ba560c5e41d16e6d89d4f4901a9fd12077b
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-09-28 00:15:07 +0300

    كفاية كدا النهارده

--- mail-server/src/store/index.js
+++ mail-server/src/store/index.js
@@ -1,7 +1,7 @@
 import {createStore} from 'vuex';
 //THIS is a single source of truth in vue js
 //We will commit user data here and it will be accessible from any part of project
-import createPersistedState from 'vuex-persistedstate';
+import createPersistedState from 'vuex-persistedstate';    //Uses local storage to persist data
 const store= createStore({
     state:{
          user:{
--- mail-server/src/views/AboutView.vue
+++ mail-server/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>welcome {{getUser.email}}</h1>
   </div>
 </template>
 
